da1dda1d661422c605e6cd1b83b30472c545c311
ishankatiyar <ishankatiyar64@gmail.com>
2025-06-01 01:00:45 +0530

    added domain url

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:5173',
+    origin:['http://localhost:5173','https://edunexus-the-lms-app.netlify.app'], 
     credentials:true 
 };
 app.use(cors(corsOption)); 
