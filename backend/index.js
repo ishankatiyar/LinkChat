a90185d16525f53a8fac3df426cd025270ed8187
ishankatiyar <ishankatiyar64@gmail.com>
2025-06-01 03:16:16 +0530

    cors issue

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:['http://localhost:5173','https://linkchat-1vxy.onrender.com'], 
+    origin:['http://localhost:5173','https://linkchat-app.onrender.com'], 
     credentials:true 
 };  
 app.use(cors(corsOption)); 
