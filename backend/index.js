112ed03a8e5bb94406c05dcb36bf5d2d87da143b
ishankatiyar <ishankatiyar64@gmail.com>
2025-06-01 01:33:34 +0530

    cors issue

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:['http://localhost:5173','https://vocal-starburst-0f59f6.netlify.app'], 
+    origin:['http://localhost:5173','https://vocal-starburst-0f59f6.netlify.app','https://edunexus-the-lms-app.netlify.app'], 
     credentials:true 
 };
 app.use(cors(corsOption)); 
